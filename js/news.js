022ae16206b4f3e9dc97fff8afbf5c42c36a9b1a
codingame120 <codinggame120@gmail.com>
2019-08-14 11:20:52 +0200

    initial commit

--- js/news.js
+++ js/news.js
@@ -3,4 +3,4 @@ import commentBox from 'commentbox.io';
 const commentBox = require('commentbox.io');
 // or if using the CDN, it will be available as a global "commentBox" variable.
 
-commentBox('my-project-id');
+commentBox('5646818905948160-proj');
